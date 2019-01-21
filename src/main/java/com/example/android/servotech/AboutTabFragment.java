f7e756db540b6e3cb7e163eadf922b795a4d3a2a
Andreea Pocol <andreeapocol@ymail.com>
2019-01-21 16:22:25 -0500

    Added fragment java files for about and contact

---
+++ src/main/java/com/example/android/servotech/AboutTabFragment.java
@@ -0,0 +1,19 @@
+package com.example.android.servotech;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AboutTabFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.about_tab_fragment,container,false);
+        return view;
+    }
+}
